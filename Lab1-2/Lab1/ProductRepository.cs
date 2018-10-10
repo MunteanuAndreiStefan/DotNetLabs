e431f0794f11f01532d6d32de732bd489d899a46
Munteanu Andrei-Stefan <munteanu.stefan@outlook.com>
2018-10-10 13:27:14 +0300

    Adding end of lab2.

---
+++ Lab1-2/Lab1/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Lab1
+{
+    public class ProductRepository
+    {
+        List<Product> products;
+
+        public ProductRepository() {
+            products = new List<Product>();
+        }
+
+        public void AddProduct(Product toAdd)
+        {
+            products.Add(toAdd);
+        }
+
+        public Product GetProductByPosition(int position)
+        {
+            return products.ElementAt(position);
+        }
+
+        public void RemoveProductByName(string toRemove)
+        {
+            products.RemoveAll(p => p.Name == toRemove);
+        }
+
+        public Product GetProductByName(string toFind)
+        {
+            return products.Find(i => i.Name == toFind);
+        }
+
+        public List<Product> FindAllProducts()
+        {
+            return products;
+        }
+    }
+}
---
+++ Lab1-2/Test/MSTests/TestProductRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab1;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test.MSTests
+{
+    [TestClass]
+    public class TestProductRepository
+    {
+
+        private static ProductRepository products;
+        private static Product pr1;
+        private static Product pr2;
+        private static Product pr3;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            products = new ProductRepository();
+            pr1 = new Product();
+            pr2 = new Product();
+            pr3 = new Product();
+            products.AddProduct(pr1);
+            products.AddProduct(pr2);
+            products.AddProduct(pr3);
+        }
+
+        [TestMethod]
+        public void TestAddProduct()
+        {
+            var prod = new ProductRepository();
+            prod.AddProduct(pr1);
+            prod.AddProduct(pr2);
+            prod.AddProduct(pr3);
+            Assert.AreEqual(products.FindAllProducts().Count, 3);
+            bool contains = products.FindAllProducts().Any(e => prod.FindAllProducts().Any(d => d.Equals(e)));
+            Assert.AreEqual(true, contains);
+        }
+
+        [TestMethod]
+        public void TestGetProductByPosition()
+        {
+            Assert.AreEqual(products.GetProductByPosition(1), pr2);
+        }
+
+        [TestMethod]
+        public void TestRemoveProductByName()
+        {
+            var prods = new ProductRepository();
+            prods.AddProduct(pr1);
+            prods.AddProduct(pr2);
+            prods.AddProduct(pr3);
+            prods.RemoveProductByName("No name");
+            Assert.AreEqual(prods.FindAllProducts().Count, 0);
+        }
+
+        [TestMethod]
+        public void TestGetProductByName()
+        {
+            Assert.AreEqual(products.GetProductByName("No name"), pr1);
+        }
+
+        [TestMethod]
+        public void TestFindAllProducts()
+        {
+            var prod = new List<Product> { pr1, pr2, pr3 };
+            bool contains = products.FindAllProducts().Any(e => prod.Any(d => d.Equals(e)));
+            Assert.AreEqual(true, contains);
+        }
+    }
+}
