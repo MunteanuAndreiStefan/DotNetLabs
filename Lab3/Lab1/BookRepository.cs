c54662e286cc4904b8c7484ab7ab31be8dc9c340
Munteanu Andrei-Stefan <munteanu.stefan@outlook.com>
2018-10-17 10:17:09 +0300

    Finish

--- Lab3/Lab1/BookRepository.cs
+++ Lab3/Lab1/BookRepository.cs
@@ -21,6 +21,21 @@ namespace Lab3
             return bookList;
         }
 
+        public List<Book> MRetriveAllOrderByYearDescending()
+        {
+            if (lastSort != 1)
+            {
+                cBookList = bookList;
+                cBookList.Sort(delegate (Book x, Book y)
+                {
+                    return x.Year.CompareTo(y.Year);
+                });
+                lastSort = 1;
+                cBookList.Reverse();
+            }
+            return cBookList;
+        }
+
         public List<Book> RetriveAllOrderByYearDescending()
         {
             if (lastSort != 1)
@@ -41,7 +56,36 @@ namespace Lab3
             return cBookList;
         }
 
+        public List<Book> MRetriveAllOrderByYearAscending()
+        {
+            if (lastSort != 2)
+            {
+                cBookList = bookList;
+                cBookList.Sort(delegate (Book x, Book y)
+                {
+                    return x.Year.CompareTo(y.Year);
+                });
+                lastSort = 2;
+            }
+            return cBookList;
+        }
+
         public List<Book> RetriveAllOrderByPriceDescending()
+        {
+            if (lastSort != 3)
+            {
+                cBookList = bookList;
+                cBookList.Sort(delegate (Book x, Book y)
+                {
+                    return x.Price.CompareTo(y.Price);
+                });
+                lastSort = 3;
+                cBookList.Reverse();
+            }
+            return cBookList;
+        }
+
+        public List<Book> MRetriveAllOrderByPriceDescending()
         {
             if (lastSort != 3)
             {
@@ -61,9 +105,34 @@ namespace Lab3
             return cBookList;
         }
 
+        public List<Book> MRetriveAllOrderByPriceAscending()
+        {
+            if (lastSort != 4)
+            {
+                cBookList = bookList;
+                cBookList.Sort(delegate (Book x, Book y)
+                {
+                    return x.Price.CompareTo(y.Price);
+                });
+                lastSort = 4;
+            }
+            return cBookList;
+        }
+
         public List<Book> RetriveAllBooksGroupedByGenre(Book.Generes genre)
         {
             List<Book> list = new List<Book>(bookList.Where(x => x.Genre == genre));
+            return list;
+        }
+
+        public List<Book> MRetriveAllBooksGroupedByGenre(Book.Generes genre)
+        {
+            List<Book> list = new List<Book>();
+            foreach (Book b in bookList)
+            {
+                if (b.Genre == genre)
+                    list.Add(b);
+            }
             return list;
         }
     }
